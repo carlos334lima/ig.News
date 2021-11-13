b7295f7a0af946e6d86a4b5c294934f1b5dcc2e8
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-11-13 16:42:31 -0300

    Posts|Prismic CMS

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Link from "next/link";
+import { GetStaticProps } from "next";
+
+//@styles
+import styles from "./styles.module.scss";
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostsProps = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | ig.News</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In The guides</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
